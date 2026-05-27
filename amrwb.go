4e91bc5f3df5deb77efb9b96557b168c59bc0783
Denys Smirnov <denys@livekit.io>
2026-05-27 11:24:53 +0200

    Clarify usage of BlockSize and add Magic.

--- amrwb.go
+++ amrwb.go
@@ -2,15 +2,17 @@
 package amrwb
 
 const (
-	PCMFrameSize = 320 // PCM frame size at 16kHz
-	FrameSizeMax = 61  // max encoded AMR-WB block size
+	Magic        = "#!AMR-WB\n" // File magic for AMR-WB, as described in RFC 4867 #5.1.
+	PCMFrameSize = 320          // PCM frame size at 16kHz
+	FrameSizeMax = 61           // max encoded AMR-WB block size
 )
 
 type PCMFrame = [PCMFrameSize]int16
 
 var blockSizes = []byte{18, 24, 33, 37, 41, 47, 51, 59, 61, 6, 6, 0, 0, 0, 1, 1}
 
-// BlockSize returns size of the first AMR-WB block in data.
+// BlockSize returns size of the first AMR-WB block in file data, as described in RFC 4867 #5.3.
+// This function is not suitable for use in parsing of AMR-WB RTP payload, as it uses a different format.
 func BlockSize(data []byte) int {
 	if len(data) == 0 {
 		return 0
@@ -26,8 +28,8 @@ func BlockSize(data []byte) int {
 	return n
 }
 
-// Blocks counts the number of AMR-WB blocks in the frame.
-// It also returns the size of all valid blocks.
+// Blocks counts the number of AMR-WB blocks in file data, as described in RFC 4867 #5.3.
+// See BlockSize for details.
 func Blocks(data []byte) (sz, cnt int) {
 	for len(data) > 0 {
 		n := BlockSize(data)
